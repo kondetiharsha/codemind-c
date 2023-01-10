69637a122bb43f219f1924e9c42453a3fa319a0f
kondetiharsha <119839744+kondetiharsha@users.noreply.github.com>
2023-01-10 19:07:48 +0530

    #include<stdio.h> int main() {     int a,b,c; scanf("%d%d%d",&a,&b,&c); if(a==b && b==c) { printf("Equilateral triangle"); } else if(a==b||a==c||b==c) { printf("Isosceles triangle"); } else if(a!=b && b!=c) { printf("Scalene triangle"); } }

---
+++ Triangle.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main()
+{ 
+    int a,b,c; scanf("%d%d%d",&a,&b,&c); if(a==b && b==c) { printf("Equilateral triangle"); } else if(a==b||a==c||b==c) { printf("Isosceles triangle"); } else if(a!=b && b!=c) { printf("Scalene triangle"); } }
