c31e6a72158d93cf100fea835e0d5c527b0024dd
kondetiharsha <119839744+kondetiharsha@users.noreply.github.com>
2023-01-22 14:05:25 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++) { scanf("%d",&a[i]); } for(i=0;i<n;i++) { if(a[i]==0) { printf("%d ",a[i]); } } for(i=0;i<n;i++) { if(a[i]==1) { printf("%d ",a[i]); } }}

---
+++ Sort_an_Array_of_0s_and_1s.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int n; 
+    scanf("%d",&n); 
+    int a[n],i; 
+    for(i=0;i<n;i++) { scanf("%d",&a[i]); } for(i=0;i<n;i++) { if(a[i]==0) { printf("%d ",a[i]); } } for(i=0;i<n;i++) { if(a[i]==1) { printf("%d ",a[i]); } }}
