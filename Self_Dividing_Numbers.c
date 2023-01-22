6399eb4964d86cf200ee5a23594ae84200fdc4c4
kondetiharsha <119839744+kondetiharsha@users.noreply.github.com>
2023-01-22 14:32:18 +0530

    #include<stdio.h> int main() {     int a,b,d,c=0,tc=0,r; scanf("%d%d",&a,&b); int i,j; for(i=a;i<=b;i++) { d=i; c=0; tc=0; while(d!=0) { c++; r=d%10; if(r!=0 && i%r==0) { tc++; } d=d/10; } if(c==tc) { printf("%d ",i); } }}

---
+++ Self_Dividing_Numbers.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,d,c=0,tc=0,r; scanf("%d%d",&a,&b); int i,j; for(i=a;i<=b;i++) { d=i; c=0; tc=0; while(d!=0) { c++; r=d%10; if(r!=0 && i%r==0) { tc++; } d=d/10; } if(c==tc) { printf("%d ",i); } }}
