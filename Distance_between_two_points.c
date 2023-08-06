603e9552a5fe4245afa8d5cbb7c1ca341dc6a9c2
kondetiharsha <119839744+kondetiharsha@users.noreply.github.com>
2023-08-07 00:09:02 +0530

    #include<iostream> #include<iomanip> #include<cmath> using namespace std; int main() {     int x1,x2,y1,y2;     float xy;     cin>>x1;     cin>>x2;     cin>>y1;     cin>>y2;     xy=sqrt(((y1-x1)*(y1-x1))+((y2-x2)*(y2-x2)));     cout<<fixed<<setprecision(4);     cout<<xy<<endl;
    }

--- Distance_between_two_points.c
+++ Distance_between_two_points.c
@@ -1,4 +1,17 @@
-#include<stdio.h>
-#include<math.h>
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
 int main()
-{ int x1,x2,y1,y2; scanf("%d%d%d%d",&x1,&x2,&y1,&y2); float f; f=sqrt((y1-x1)*(y1-x1)+(y2-x2)*(y2-x2)); printf("%.4f",f);}
+{
+    int x1,x2,y1,y2;
+    float xy;
+    cin>>x1;
+    cin>>x2;
+    cin>>y1;
+    cin>>y2;
+    xy=sqrt(((y1-x1)*(y1-x1))+((y2-x2)*(y2-x2)));
+    cout<<fixed<<setprecision(4);
+    cout<<xy<<endl;
+    
+}
