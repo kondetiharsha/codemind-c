a5e253673b05b0be39435ed10f7d79c947ffa215
kondetiharsha <119839744+kondetiharsha@users.noreply.github.com>
2023-08-06 23:30:56 +0530

    #include<iostream> #include<iomanip> using namespace std; int main() {     int a,b;     int c,d,e,f,g;     cin>>a; 	cin>>b;     c=a+b;     d=a-b;     e=a*b;     f=a/b;     g=a%b;     cout<<"Sum:"<<c<<endl;     cout<<"Difference:"<<d<<endl;     cout<<"Product:"<<e<<endl;     cout<<"Quotient:"<<f<<endl;     cout<<"Remainder:"<<g<<endl; }

--- Arithmetic_operations.c
+++ Arithmetic_operations.c
@@ -1,6 +1,20 @@
-#include<stdio.h>
-int main(){ int a,b; scanf("%d%d",&a,&b); int c,d,e,f,g; c=a+b; d=a-b; e=a*b; f=a/b; g=a%b; printf("Sum:%d
-",c); printf("Difference:%d
-",d); printf("Product:%d
-",e); printf("Quotient:%d
-",f); printf("Remainder:%d",g); }
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int a,b;
+    int c,d,e,f,g;
+    cin>>a;
+	cin>>b;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    g=a%b;
+    cout<<"Sum:"<<c<<endl;
+    cout<<"Difference:"<<d<<endl;
+    cout<<"Product:"<<e<<endl;
+    cout<<"Quotient:"<<f<<endl;
+    cout<<"Remainder:"<<g<<endl;
+}
