8606220c1bbcc70404505780cab3ead35912ebb1
kondetiharsha <119839744+kondetiharsha@users.noreply.github.com>
2023-08-06 23:12:38 +0530

    #include<iostream> #include<iomanip> using namespace std; int main() {     int n;     float f;     cin>>n;     f=2.54*n;     cout<<fixed<<setprecision(2);     cout<<f<<endl;     return 0; }

--- inches_to_centimeters.c
+++ inches_to_centimeters.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include<iostream>
+#include<iomanip>
+using namespace std;
 int main()
-{ 
-    int h; 
-    scanf("%d",&h); 
-    float f; 
-    f=2.54*h;
-    printf("%.2f",f);
-    
+{
+    int n;
+    float f;
+    cin>>n;
+    f=2.54*n;
+    cout<<fixed<<setprecision(2);
+    cout<<f<<endl;
+    return 0;
 }
